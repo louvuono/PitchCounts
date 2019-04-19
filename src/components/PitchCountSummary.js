3edcdd403ffd6ed32df96181d3b53d08282092df
Lou Vuono <lvuono15@gmail.com>
2019-04-19 09:13:59 -0400

    Fix build

--- src/components/PitchCountSummary.js
+++ src/components/PitchCountSummary.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import numeral from 'numeral';
-import selectPitchCounts from '../selectors/PitchCounts';
-import selectPitchCountsTotal from '../selectors/PitchCountsTotal';
+import selectPitchCounts from '../selectors/pitchcounts';
+import selectPitchCountsTotal from '../selectors/pitchcountsTotal';
 
 export const PitchCountSummary = ({ PitchCountCount, PitchCountsTotal }) => {
     return (
